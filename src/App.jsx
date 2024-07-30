2132bcafad9d02dc7af3e290718823d31a9ac14c
Andres Morales <moralesandres@outlook.com>
2024-07-30 13:06:48 -0500

    7.7 update

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ const Country = ({ country }) => {
       <h3>{country.data.name.common} </h3>
       <div>capital {country.data.capital} </div>
       <div>population {country.data.population}</div>
-      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name}`} />
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`} />
     </div>
   )
 }
